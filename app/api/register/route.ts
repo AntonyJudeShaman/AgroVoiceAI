296310dd71f2a00eddebcd1cd260646c0f8f2707
Antony Jude Shaman <antonyjudeshaman@gmail.com>
2024-02-22 09:41:28 +0530

    Add bcrypt dependency and update user creation logic

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,12 @@
-import prisma from "@/lib/prisma";
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { hash } from "bcrypt";
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
-  const exists = await prisma.user.findUnique({
+  const exists = await db.user.findUnique({
     where: {
       email,
     },
@@ -13,7 +14,7 @@ export async function POST(req: Request) {
   if (exists) {
     return NextResponse.json({ error: "User already exists" }, { status: 400 });
   } else {
-    const user = await prisma.user.create({
+    const user = await db.user.create({
       data: {
         email,
         password: await hash(password, 10),
