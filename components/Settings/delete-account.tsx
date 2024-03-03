c727deb2e9fc790554f4260e869a04ce2a8b0f93
Antony Jude Shaman <antonyjudeshaman@gmail.com>
2024-03-03 20:42:27 +0530

    style: change button color

--- components/Settings/delete-account.tsx
+++ components/Settings/delete-account.tsx
@@ -53,7 +53,7 @@ export function DeleteAccount() {
                 type="button"
                 className={cn(
                   buttonVariants(),
-                  'bg-red-700 text-white border dark:border-gray-700/70 flex justify-center items-center hover:bg-red-600/60 hover:text-white hover:border border-red-500'
+                  'bg-green-700 text-white border dark:border-green-200/70 flex justify-center items-center hover:bg-red-600/60 hover:text-white hover:border border-green-900'
                 )}
                 size="lg"
                 disabled={isSaving}
