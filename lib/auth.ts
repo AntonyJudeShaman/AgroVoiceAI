75f80736baa16cacfba060c1652b39c052debfbb
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-03-24 20:08:58 +0530

    Update next-auth version and refactor authentication logic

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,9 +4,7 @@ import Credentials from 'next-auth/providers/credentials'
 
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { db } from './db'
-import { getStringFromBuffer } from './utils'
-import toast from 'react-hot-toast'
-import { NextResponse } from 'next/server'
+import { hashPassword } from './utils'
 
 declare module 'next-auth' {
   interface Session {
@@ -28,9 +26,9 @@ export const {
       authorize: async credentials => {
         console.log('inside authorize')
         console.log(credentials)
-        const user = await db.user.findFirst({
+        const user = await db.user.findUnique({
           where: {
-            email: credentials.email as string
+            userName: credentials.name as string
           }
         })
 
@@ -43,19 +41,19 @@ export const {
           'SHA-512',
           saltedPassword
         )
-        const hashedPassword = getStringFromBuffer(hashedPasswordBuffer)
+        const hashedPassword = await hashPassword(
+          credentials.password as string
+        )
         if (user) {
           if (hashedPassword === user?.password) {
             console.log('User found')
             return user
           } else {
             console.error('Invalid credentials')
-            // throw new Error('Invalid credentials')
             return null
           }
         } else {
           console.error('User not found')
-          // throw new Error('User not found')
           return null
         }
       }
@@ -75,9 +73,10 @@ export const {
       return session
     },
     async jwt({ token, user }) {
+      console.log('inside jwt')
       const dbUser = await db.user.findFirst({
         where: {
-          email: token.email
+          userName: token.email
         }
       })
 
