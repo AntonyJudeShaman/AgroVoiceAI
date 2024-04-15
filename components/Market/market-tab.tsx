769e34daa6a1a6abcd7a46729ad616a1a316b517
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-04-15 23:45:24 +0530

    chore: localized text for market tabs

--- components/Market/market-tab.tsx
+++ components/Market/market-tab.tsx
@@ -3,26 +3,34 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { getTranslations } from 'next-intl/server'
 import MarketHome2 from './market-home-api-2'
 import MarketHomeFruits from './market-home-fruits'
+import { useLocale } from 'next-intl'
 
 export async function MarketsTab() {
   const t = await getTranslations('Index')
   const user = await getCurrentUser()
-  
+  const locale = useLocale()
+
   return (
     <Tabs defaultValue="vegetables" className="w-full px-4">
       <TabsList className="grid font-pops md:w-[80%] 2xl:w-[70%] mx-auto grid-cols-3 border dark:bg-black bg-white border-gray-500 dark:border-slate-700">
-        <TabsTrigger value="vegetables">Vegetables</TabsTrigger>
-        <TabsTrigger value="fruits">Fruits</TabsTrigger>
-        <TabsTrigger value="graph">Graph</TabsTrigger>
+        <TabsTrigger value="vegetables">
+          {locale === 'en' ? 'Vegetables' : 'காய்கறிகள்'}
+        </TabsTrigger>
+        <TabsTrigger value="fruits">
+          {locale === 'en' ? 'Fruits' : 'பழங்கள்'}
+        </TabsTrigger>
+        <TabsTrigger value="graph">
+          {locale === 'en' ? 'Graph' : 'வரைபடம்'}
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="vegetables">
-        <MarketHome2 user={user}/>
+        <MarketHome2 user={user} />
       </TabsContent>
       <TabsContent value="fruits">
-      <MarketHomeFruits user={user}/>
+        <MarketHomeFruits user={user} />
       </TabsContent>
       <TabsContent value="graph">
-      <MarketHomeFruits user={user}/>
+        <MarketHomeFruits user={user} />
       </TabsContent>
     </Tabs>
   )
